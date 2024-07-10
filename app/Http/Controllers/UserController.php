794ca0b0f43387298f37bfe16be1bef523036ae5
akmalfman <akmalfsalman2@gmail.com>
2024-07-10 17:57:36 +0700

    revisi login register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,11 @@ use App\Models\User;
 
 class UserController extends Controller
 {
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
     public function login()
     {
         return view('user.login');
